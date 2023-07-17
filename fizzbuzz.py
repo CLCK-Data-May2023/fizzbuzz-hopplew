8512626cff14343948867d4a9b5cf035a8015b59
Will Anderson <hopplew@gmail.com>
2023-07-17 18:35:21 -0400

    fizzbuzz code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
-# add your code here
+number = range(1, 101)
 
+for n in number:
+    if n % 3 == 0 and n % 5 == 0:
+        print('FizzBuzz')
+    elif n % 3 == 0:
+        print('Fizz')
+    elif n % 5 == 0:
+        print('Buzz')
+    else:
+        print(n)
